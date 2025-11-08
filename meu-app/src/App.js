cb2da7bd804db464619cd30c20e1ecc0482de47e
andrey.rebelatto <lzzgrip@gmail.com>
2025-11-08 13:20:31 -0300

    cmt-08112025

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,14 +1,39 @@
 import './App.css';
 import Header from "./Header.js";
 import Counter from "./Counter.js";
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div>
-      <Header name="Nome do Header" links= {["About", "Buy", "Contact", "Login"]}></Header>
-      <Counter count={5}></Counter>
-    </div>
-  );
+
+  const [show, setShow] = useState(true);
+  let time = 0;
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+        time++;
+        console.log(time);
+        if(time > 5){
+          clearInterval(timer);
+          setShow(false)
+        }
+      }, 1000);
+  }, [])
+
+
+  if(show){
+    return (
+      <div>
+        {/* <Header name="Nome do Header" links= {["About", "Buy", "Contact", "Login"]}></Header> */}
+        <Counter count={0}></Counter>
+      </div>
+    );
+  }else{
+    return (
+      <div>
+        Outra div
+      </div>
+    );
+  }
 }
 
 export default App;
--- meu-app/src/Counter.js
+++ meu-app/src/Counter.js
@@ -1,29 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class Counter extends React.Component {
+function Counter(props){
 
-    constructor(props){
-        super(props);
+    // Utilizando hooks
+    const [count, setCount] = useState(() => {
+        const storedCount = localStorage.getItem("count");
 
-        this.state = {count: this.props.count}
-        this.add = this.add.bind(this)
+        if(storedCount){
+            return parseInt(localStorage.getItem("count"));
+        }
+
+        return props.count || 0;
+    });
+
+    useEffect(() => {
+        setCount(parseInt(localStorage.getItem("count")));
+
+        return () => {
+            console.log("Executado ao remover component")
+        }
+    }, []) // Array vazio para o effect nao monitorar nenhuma variavel
+
+    useEffect(() => {
+        document.title = count;
+        localStorage.setItem("count", count);
+    }, [count])
+
+    function add(){
+        setCount(count + 1);
     }
 
-    add(){
-        // this.setState({count: this.state.count + 1})
-        this.setState((state) => {
-            return {count: state.count + 1}
-        })
-    }
-
-    render(){
-        return (
-            <div>
-                <h1>Counter: {this.state.count}</h1>
-                <button onClick={this.add}>add</button>
-            </div>
-        )
-    }
+    return(
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={add}>add</button>
+        </div>
+    )
 }
+
+// class Counter extends React.Component {
+
+//     // Primeiro metodo do ciclo de vida de um component
+//     constructor(props){
+//         super(props);
+
+//         this.state = { count: 0 }
+//         this.add = this.add.bind(this)
+//     }
+
+//     add(){
+//         // this.setState({count: this.state.count + 1})
+//         this.setState((state) => {
+//             localStorage.setItem("state", JSON.stringify(this.state));
+//             return {count: state.count + 1}
+//         }, () => {
+//             console.log(this.state);
+//         })
+//     }
+
+//     // Executado depois que um coponent for carregado em tela
+//     componentDidMount(){
+//         this.setState(JSON.parse(localStorage.getItem("state")));
+//     }
+
+//     // Chamado quando o component for removido da tela
+//     componentWillUnmount(){
+
+//     }
+
+//     // Informa se um component deve ser atualizado, se falso, ele nao sofre upd
+//     // shouldComponentUpdate(){
+//     //     let stateCount = this.state.count
+//     //     if(stateCount >= 5){
+//     //         return true;
+//     //     }else{
+//     //         return false;
+//     //     }
+//     // }
+
+//     // Metódo que desenha na tela o component conforme a alteração de estado
+//     render(){
+//         return (
+//             <div>
+//                 <h1>Counter: {this.state.count}</h1>
+//                 <button onClick={this.add}>add</button>
+//             </div>
+//         )
+//     }
+// }
 
 export default Counter;
